01bcd26c55cbfb601e84667dfe6922e0253184b5
Erik Zapata <zapatabrandon@gmail.com>
2016-10-17 21:16:30 -0400

    Init commit with hopefully correct project settings. Change the properties once you've pulled this commit

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+/*
+	Testing this shit out to see if i can get this to work
+*/
+
+#include <iostream>
+#include <cstring>
+#include "Leap.h"
+
+using namespace Leap;
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
